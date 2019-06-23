bb56b9e72517747a0df3b94a8269e42e3631ae5e
heweigeng1 <6318275@qq.com>
2019-06-23 23:33:29 +0800

    socket

---
+++ AbpSignalr_Client/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AbpSignalr_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.UrlTxt = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tenantCodeTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(856, 146);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(99, 39);
+            this.start.TabIndex = 0;
+            this.start.Text = "开始监听";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // UrlTxt
+            // 
+            this.UrlTxt.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.UrlTxt.Location = new System.Drawing.Point(455, 25);
+            this.UrlTxt.Name = "UrlTxt";
+            this.UrlTxt.Size = new System.Drawing.Size(500, 39);
+            this.UrlTxt.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 213);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(943, 667);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // tenantCodeTxt
+            // 
+            this.tenantCodeTxt.Font = new System.Drawing.Font("宋体", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tenantCodeTxt.Location = new System.Drawing.Point(455, 90);
+            this.tenantCodeTxt.Name = "tenantCodeTxt";
+            this.tenantCodeTxt.Size = new System.Drawing.Size(500, 39);
+            this.tenantCodeTxt.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "监听地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "监听店铺";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 892);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tenantCodeTxt);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.UrlTxt);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "直管门店订单监视器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.TextBox UrlTxt;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox tenantCodeTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AbpSignalr_Client/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace AbpSignalr_Client
+{
+    public partial class Form1 : Form
+    {
+        //定义代理,广播服务连接相关
+        private static IHubProxy HubProxy { get; set; }
+        //private static readonly string ServerUrl = ConfigurationManager.AppSettings["signalrServer"];
+        //private static string _serverUrl ="";
+        //定义一个连接对象
+        public static HubConnection Connection { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            var url = this.UrlTxt.Text;
+            var tenant = this.tenantCodeTxt.Text.Split(',');
+            Console.WriteLine("连接服务器成功");
+            if (!string.IsNullOrEmpty(url))
+            {
+                var Connection = new HubConnection(url);
+                HubProxy = Connection.CreateHubProxy("orderpaycall");
+                try
+                {
+                    Connection.Start().Wait();
+                    var sound = new SoundPlayer
+                    {
+                        SoundLocation = @"C:\Windows\media\Alarm06.wav"
+                    };
+                    sound.Play();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+        }
+    }
+}
